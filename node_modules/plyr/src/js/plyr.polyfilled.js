00032ec526a54270f35cb99d3952594991d6456f
Zishu Zhao <zishu.zhao@ibm.com@Zishus-MacBook-Pro-4.local>
2019-06-07 13:36:25 +0100

    over ride

---
+++ node_modules/plyr/src/js/plyr.polyfilled.js
@@ -0,0 +1,12 @@
+// ==========================================================================
+// Plyr Polyfilled Build
+// plyr.js v3.5.4
+// https://github.com/sampotts/plyr
+// License: The MIT License (MIT)
+// ==========================================================================
+
+import 'custom-event-polyfill';
+import 'url-polyfill';
+import Plyr from './plyr';
+
+export default Plyr;
